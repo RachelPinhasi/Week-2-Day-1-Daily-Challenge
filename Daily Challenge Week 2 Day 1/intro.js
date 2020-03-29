9cec14f31fd5ec40252dc4789fdeb80314f3e9a2
RachelPinhasi <62834344+RachelPinhasi@users.noreply.github.com>
2020-03-29 23:39:40 +0300

    Add files via upload

---
+++ Daily Challenge Week 2 Day 1/intro.js
@@ -0,0 +1,30 @@
+let array = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+//#1
+
+array.shift();
+
+//#2
+
+array.sort();
+
+//#3
+array.push("Kiwis");
+
+//#4
+
+array.shift();
+
+//#5
+
+array.reverse();
+
+
+
+
+let array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+array2.indexOf("Oranges");
+
+
+
